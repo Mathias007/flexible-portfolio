b6287c07a5679f4c24d19ba5101a38a20f942fd5
Mathias007 <mathias.najlepszy@gmail.com>
2024-02-10 23:02:23 +0100

    [FE] Create components for Navigation section.

---
+++ frontend/src/components/Navigation/index.tsx
@@ -0,0 +1,4 @@
+export { default as Navigation } from "./Navigation";
+export { default as NavigationItem } from "./NavigationItem";
+export { default as NavigationLogo } from "./NavigationLogo";
+export { default as NavigationToggler } from "./NavigationToggler";
