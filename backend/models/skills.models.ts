f8b0d11cb235756c3d11250f21690da4cd23220d
Mathias007 <mathias.najlepszy@gmail.com>
2024-02-13 19:36:55 +0100

    [Backend] Add SkillsDataModel.

--- backend/models/skills.models.ts
+++ backend/models/skills.models.ts
@@ -1,5 +1,20 @@
+import mongoose from "mongoose";
+
 export interface ISkillsData {
     id: number;
     skill: string;
     value: number;
 }
+
+const SkillsDataSchema = new mongoose.Schema({
+    id: { type: Number, required: true },
+    skill: { type: String, required: true },
+    value: { type: Number, required: true },
+});
+
+const SkillsDataModel = mongoose.model<ISkillsData>(
+    "SkillsData",
+    SkillsDataSchema
+);
+
+export default SkillsDataModel;
