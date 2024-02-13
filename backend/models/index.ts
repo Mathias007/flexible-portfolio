19180b81ce87e21b879683e5d0368b64f26607e1
Mathias007 <mathias.najlepszy@gmail.com>
2024-02-13 19:37:09 +0100

    [BE] Update index in models.

--- backend/models/index.ts
+++ backend/models/index.ts
@@ -1,7 +1,15 @@
+export { default as ContactDataModel } from "./contact.models";
+export { default as PersonalDataModel } from "./personal.models";
+export { default as PortfolioDataModel } from "./portfolio.models";
+export { default as ToolsDataModel } from "./tools.models";
+export { default as SkillsDataModel } from "./skills.models";
+export { default as SocialDataModel } from "./social.models";
+export { default as TimelineDataModel } from "./timeline.models";
+
 import * as ContactModels from "./contact.models";
 import * as PersonalModels from "./personal.models";
 import * as PortfolioModels from "./portfolio.models";
-import * as ServicesModels from "./services.models";
+import * as ToolsModels from "./tools.models";
 import * as SkillsModels from "./skills.models";
 import * as SocialModels from "./social.models";
 import * as TimelineModels from "./timeline.models";
@@ -10,8 +18,8 @@ export {
     ContactModels,
     PersonalModels,
     PortfolioModels,
-    ServicesModels,
+    ToolsModels,
     SkillsModels,
     SocialModels,
-    TimelineModels
+    TimelineModels,
 };
