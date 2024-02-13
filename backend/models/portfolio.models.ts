840f4d2d5c8926e3f98b546049dcc43e0a74a189
Mathias007 <mathias.najlepszy@gmail.com>
2024-02-13 19:35:58 +0100

    [Backend] Add PortfolioDataModel.

--- backend/models/portfolio.models.ts
+++ backend/models/portfolio.models.ts
@@ -1,5 +1,20 @@
+import mongoose from "mongoose";
+
 export interface IPortfolioData {
     id: number;
     logo: string;
     description: string;
 }
+
+const PortfolioDataSchema = new mongoose.Schema({
+    id: { type: Number, required: true },
+    logo: { type: String, required: true },
+    description: { type: String, required: true },
+});
+
+const PortfolioDataModel = mongoose.model<IPortfolioData>(
+    "PortfolioData",
+    PortfolioDataSchema
+);
+
+export default PortfolioDataModel;
