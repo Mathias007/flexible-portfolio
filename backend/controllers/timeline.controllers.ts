9cb7590bfb69bae9f061a9e7a1c1564b4867fb4f
Mathias007 <mathias.najlepszy@gmail.com>
2024-02-14 22:32:32 +0100

    [BE] Use ServerStatuses and ServerMessages in controllers.

--- backend/controllers/timeline.controllers.ts
+++ backend/controllers/timeline.controllers.ts
@@ -1,6 +1,8 @@
 import { Request, Response } from "express";
 import { TimelineService } from "../services";
 
+import { ServerStatuses, ServerMessages } from "../config";
+
 class TimelineController {
     private timelineService: TimelineService;
 
@@ -14,7 +16,9 @@ class TimelineController {
             res.json(timelineData);
         } catch (error) {
             console.error(error);
-            res.status(500).json({ message: "Internal server error" });
+            res.status(ServerStatuses.INTERNAL_ERROR).json({
+                message: ServerMessages.INTERNAL_ERROR,
+            });
         }
     };
 }
