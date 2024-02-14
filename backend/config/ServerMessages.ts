efcf23204c5e7099fcf17b98ccb627a8000717dc
Mathias007 <mathias.najlepszy@gmail.com>
2024-02-14 22:31:07 +0100

    [BE] Create ServerMessages config file. Update indexation.

---
+++ backend/config/ServerMessages.ts
@@ -0,0 +1,6 @@
+const ServerMessages = {
+    FAILED_TO_FETCH: (dataType: string) => `Failed to fetch ${dataType}`,
+    INTERNAL_ERROR: "Internal server error",
+};
+
+export default ServerMessages;
