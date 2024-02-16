3bc6b4b1b9aaf9ce52a69c800bedf0659d4f1363
Mathias007 <mathias.najlepszy@gmail.com>
2024-02-16 22:52:23 +0100

    [FE] Extract Timeline into the separate section.

---
+++ frontend/src/components/Timeline/TimelineTitle.tsx
@@ -0,0 +1,14 @@
+import { FC } from "react";
+
+const TimelineTitle: FC = () => {
+    return (
+        <div className="row" data-testid="about-title">
+            <div className="section-title padd-15">
+                <h2>Timeline</h2>
+            </div>
+        </div>
+    );
+};
+
+export default TimelineTitle;
+
---
+++ frontend/src/components/Timeline/index.tsx
@@ -0,0 +1,3 @@
+export { default as TimelineSection } from "./TimelineSection";
+export { default as TimelineTitle } from "./TimelineTitle";
+export { default as TimelineItem } from "./TimelineItem";
