4a3683f7890f0882b6f59bbe3ec8129ed5429173
Mathias007 <mathias.najlepszy@gmail.com>
2024-02-17 19:44:53 +0100

    [FE] Rebuild PortfolioItem to handle data fetched from Github API.

--- frontend/src/components/Portfolio/PortfolioItem.tsx
+++ frontend/src/components/Portfolio/PortfolioItem.tsx
@@ -1,17 +1,45 @@
 import { FC } from "react";
+import { Link } from "react-router-dom";
+import { portfolioData } from "../../data";
 
 interface IPortfolioItemProps {
-    logo: string;
     description: string;
+    homepage?: string;
+    html_url: string;
+    language: string;
+    name: string;
 }
 
-const PortfolioItem: FC<IPortfolioItemProps> = ({ logo, description }) => {
+const PortfolioItem: FC<IPortfolioItemProps> = ({
+    description,
+    homepage,
+    html_url,
+    language,
+    name,
+}) => {
     return (
         <div className="portfolio-item padd-15" data-testid="portfolio-item">
             <div className="portfolio-item-inner shadow-dark">
-                <div className="portfolio-img">
-                    <img src={logo} alt={description} />
+                <h4>
+                    {name} ({language})
+                </h4>
+                <p>{description}</p>
+                <div
+                    className="buttons padd-15"
+                    data-testid="portfolio-project-buttons"
+                >
+                    <Link to={html_url} className="btn">
+                        {portfolioData.portfolioBourceButtonLabel}
+                    </Link>
+                    {homepage && (
+                        <Link to={homepage} className="btn hire-me">
+                            {portfolioData.portfolioPreviewButtonLabel}
+                        </Link>
+                    )}
                 </div>
+                {/* <div className="portfolio-img"> */}
+                {/* <img src={logo} alt={description} /> */}
+                {/* </div> */}
             </div>
         </div>
     );
