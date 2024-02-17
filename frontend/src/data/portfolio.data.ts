5b815ce459f03a4e5807ea333e8125a8ed62abd1
Mathias007 <mathias.najlepszy@gmail.com>
2024-02-17 19:45:25 +0100

    [FE] Extract portfolio data into the portfolioData object.

---
+++ frontend/src/data/portfolio.data.ts
@@ -0,0 +1,8 @@
+const portfolioData = {
+    portfolioProjectsHeading: "Last projects published on my GitHub:",
+    portfolioBourceButtonLabel: "Source Code",
+    portfolioPreviewButtonLabel: "Preview",
+    portfolioMoreButtonLabel: "See More"
+};
+
+export default portfolioData;
