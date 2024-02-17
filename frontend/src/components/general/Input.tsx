7cd227e105cdea7be9e33387a8ba37f9da15a474
Mathias007 <mathias.najlepszy@gmail.com>
2024-02-17 19:42:17 +0100

    [FE] Prepare FormField generic component and update Button generic component.

--- frontend/src/components/general/Input.tsx
+++
@@ -1,7 +0,0 @@
-import { FC } from "react";
-
-const Input: FC = () => {
-    return <div>Input</div>;
-};
-
-export default Input;
--- frontend/src/components/general/index.tsx
+++ frontend/src/components/general/index.tsx
@@ -1,5 +1,5 @@
 export { default as Button } from "./Button";
 export { default as Icon } from "./Icon";
-export { default as Input } from "./Input";
+export { default as FormField } from "./FormField";
 export { default as StyleSwitcher } from "./StyleSwitcher";
 export { default as Title } from "./Title";
