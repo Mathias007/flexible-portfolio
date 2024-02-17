d9065b5220c9dbe734a6ae3badc0b2804313722f
Mathias007 <mathias.najlepszy@gmail.com>
2024-02-17 19:43:37 +0100

    [FE] Extract some strings into the contactData object.

--- frontend/src/data/contact.data.ts
+++ frontend/src/data/contact.data.ts
@@ -3,6 +3,12 @@ const contactData = {
     contactLinksSubtitle: "I am available in various destinations",
     contactFormTitle: "Leave a message",
     contactFormSubtitle: "I remain open to questions and offers",
+    contactFormLoadingMessage: "Loading",
+    contactFormSuccessMessage: "The Message has been sent!",
+    contactFormEmptyMessage: "All fields must be filled!",
+    contactFormFailedMessage:
+        "Something failed and the message has not been sent.",
+    contactFormButtonLabel: "Send message",
 };
 
 export default contactData;
